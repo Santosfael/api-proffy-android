59274b906756c72f412567478cc6370f3259a9d5
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-11-18 21:57:32 -0300

    feat: criando as tabelas de conexões, agendamentos de classes e classes

---
+++ drizzle/0001_common_black_cat.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "class_schedule" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"week_day" integer NOT NULL,
+	"from" integer NOT NULL,
+	"to" integer NOT NULL,
+	"class_id" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "classes" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"subject" text NOT NULL,
+	"cost" numeric NOT NULL,
+	"user_id" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "connections" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "class_schedule" ADD CONSTRAINT "class_schedule_class_id_classes_id_fk" FOREIGN KEY ("class_id") REFERENCES "public"."classes"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE "classes" ADD CONSTRAINT "classes_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE "connections" ADD CONSTRAINT "connections_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE cascade;
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,5 @@
-import { text, pgTable, uuid, pgEnum, timestamp } from "drizzle-orm/pg-core";
+import { integer } from "drizzle-orm/pg-core";
+import { text, pgTable, uuid, pgEnum, timestamp, decimal } from "drizzle-orm/pg-core";
 
 export const userRole = pgEnum("user_role", [
     "student",
@@ -16,4 +17,29 @@ export const users = pgTable("users", {
     role: userRole().default('student'),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updateAt: timestamp("updated_at")
+})
+
+export const classes = pgTable("classes", {
+    id: uuid().primaryKey().defaultRandom(),
+    subject: text().notNull(),
+    cost: decimal().notNull(),
+    userId: uuid("user_id").notNull().references(() => users.id, {onDelete: "cascade", onUpdate: "cascade"}),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at"),
+})
+
+export const classSchedule = pgTable("class_schedule", {
+    id: uuid().primaryKey().defaultRandom(),
+    weekDay: integer("week_day").notNull(),
+    from: integer().notNull(),
+    to: integer().notNull(),
+    classId: uuid("class_id").notNull().references(() => classes.id, {onDelete: "cascade", onUpdate: "cascade"}),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at"),
+})
+
+export const connections = pgTable("connections", {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid("user_id").notNull().references(() => users.id, {onDelete: "cascade", onUpdate: "cascade"}),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
 })
